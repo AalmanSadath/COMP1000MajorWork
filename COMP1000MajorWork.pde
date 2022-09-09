bd4839dee75cb674fc04ed65cb05502ac96bd639
AwaerSuperCC <arduinoatwork@gmail.com>
2022-09-09 20:13:23 +1000

    GenTrees is messed up

--- COMP1000MajorWork.pde
+++ COMP1000MajorWork.pde
@@ -1,7 +1,8 @@
-float xcord=250;
-float yenemy=-20;
-float xenemy=250;
+float xCord=250;
+float yEnemy=-20;
+float xEnemy=250;
 float walkRate=0;
+float yTree=0;
 
 void setup() {
     size(500,750);
@@ -9,14 +10,19 @@ void setup() {
 
 
 void draw() {
-    grass();
+    //grass();
+    genTree();
     wallysWalk();
+    yTree++;
+    if (yTree>=height/3) {
+        yTree=0;
+    }
     walkRate++;
     if (walkRate>=50) {
         walkRate=0;
     }
     player();
-    yenemy=yenemy+5;
+    yEnemy=yEnemy+3;
     ped();
     if (collisionPed() == true) {
         fill(100,100,100,98);
@@ -40,47 +46,46 @@ void player() {
     strokeWeight(3);
     fill(100);
     rectMode(CENTER);
-    rect(xcord,675,30,100,20);
+    rect(xCord,675,30,100,20);
     //personarms
     fill(0,150,0);
     strokeWeight(1);
     rectMode(CORNER);
-    rect(xcord-4,638,8,15,3);
-    rect(xcord-4,698,8,15,3);
+    rect(xCord-4,638,8,15,3);
+    rect(xCord-4,698,8,15,3);
     //personbody
-    rect(xcord-10,650,20,50,10);
+    rect(xCord-10,650,20,50,10);
     //head
-    circle(xcord-3,675,25);
+    circle(xCord-3,675,25);
 }
 
 void ped() {
     strokeWeight(2);
     fill(200,100,100);
     rectMode(CENTER);
-    rect(xenemy,yenemy,50,20,10);
+    rect(xEnemy,yEnemy,50,20,10);
     rectMode(CORNER);
-    circle(xenemy,yenemy+3,25);
-    if (yenemy > 750) {
-        yenemy=-100;
+    fill(200,100,100,99);
+    circle(xEnemy,yEnemy+3,25);
+    if (yEnemy > 750) {
+        yEnemy=-100;
     }
 }
 
 void keyPressed() {
     if (keyCode == 37) {
-        xcord=constrain(xcord-5,115,385);
+        xCord=constrain(xCord-5,115,385);
     }
     else if (keyCode == 39) {
-        xcord=constrain(xcord+5,115,385);
+        xCord=constrain(xCord+5,115,385);
     }
 }
 
 boolean collisionPed() {
-    float distX=abs(xcord-xenemy);
-    float distY=abs(675-yenemy);
-    if (distX <= 40) {
-        if (distY <= 60) {    
-            return true;
-        }
+    float distX=abs(xCord-xEnemy);
+    float distY=abs(675-yEnemy);
+    if (distX <= 40 && distY <= 60) {
+        return true;
     }
     return false;
 }
@@ -93,4 +98,14 @@ void wallysWalk() {
             rect(width/5+(columns*50),walkRate+(50*rows)-50,50,50);
         }
     }
+}
+
+void genTree() {
+    rectMode(CENTER);
+    fill(0,155,0);
+    strokeWeight(2);
+    rect(width/10,yTree-width/6,width/5,height/3);
+    rect(width/10,yTree+width/6,width/5,height/3);
+    rect(width/10,yTree+height/2,width/5,height/3);
+    rect(width/10,yTree+(height*5/6),width/5,height/3);
 }
--- out/source/COMP1000MajorWork.java
+++ out/source/COMP1000MajorWork.java
@@ -14,10 +14,11 @@ import java.io.IOException;
 
 public class COMP1000MajorWork extends PApplet {
 
-float xcord=250;
-float yenemy=-20;
-float xenemy=250;
+float xCord=250;
+float yEnemy=-20;
+float xEnemy=250;
 float walkRate=0;
+float yTree=0;
 
 public void setup() {
     
@@ -26,16 +27,21 @@ public void setup() {
 
 public void draw() {
     grass();
+    //genTree();
     wallysWalk();
+    yTree++;
+    if (yTree>=height/3) {
+        yTree=0;
+    }
     walkRate++;
     if (walkRate>=50) {
         walkRate=0;
     }
     player();
-    yenemy=yenemy+5;
+    yEnemy=yEnemy+3;
     ped();
-    if (collision() == true) {
-        fill(100,100,100,99);
+    if (collisionPed() == true) {
+        fill(100,100,100,98);
         rect(0,0,width,height);
         textAlign(CENTER);
         fill(0);
@@ -56,47 +62,46 @@ public void player() {
     strokeWeight(3);
     fill(100);
     rectMode(CENTER);
-    rect(xcord,675,30,100,20);
+    rect(xCord,675,30,100,20);
     //personarms
     fill(0,150,0);
     strokeWeight(1);
     rectMode(CORNER);
-    rect(xcord-4,638,8,15,3);
-    rect(xcord-4,698,8,15,3);
+    rect(xCord-4,638,8,15,3);
+    rect(xCord-4,698,8,15,3);
     //personbody
-    rect(xcord-10,650,20,50,10);
+    rect(xCord-10,650,20,50,10);
     //head
-    circle(xcord-3,675,25);
+    circle(xCord-3,675,25);
 }
 
 public void ped() {
     strokeWeight(2);
     fill(200,100,100);
     rectMode(CENTER);
-    rect(xenemy,yenemy,50,20,10);
+    rect(xEnemy,yEnemy,50,20,10);
     rectMode(CORNER);
-    circle(xenemy,yenemy+3,25);
-    if (yenemy > 750) {
-        yenemy=-100;
+    fill(200,100,100,99);
+    circle(xEnemy,yEnemy+3,25);
+    if (yEnemy > 750) {
+        yEnemy=-100;
     }
 }
 
 public void keyPressed() {
     if (keyCode == 37) {
-        xcord=constrain(xcord-5,115,385);
+        xCord=constrain(xCord-5,115,385);
     }
     else if (keyCode == 39) {
-        xcord=constrain(xcord+5,115,385);
+        xCord=constrain(xCord+5,115,385);
     }
 }
 
-public boolean collision() {
-    float distX=abs(xcord-xenemy);
-    float distY=abs(675-yenemy);
-    if (distX <= 40) {
-        if (distY <= 60) {    
-            return true;
-        }
+public boolean collisionPed() {
+    float distX=abs(xCord-xEnemy);
+    float distY=abs(675-yEnemy);
+    if (distX <= 40 && distY <= 60) {
+        return true;
     }
     return false;
 }
@@ -109,6 +114,16 @@ public void wallysWalk() {
             rect(width/5+(columns*50),walkRate+(50*rows)-50,50,50);
         }
     }
+}
+
+public void genTree() {
+    rectMode(CENTER);
+    fill(0,155,0);
+    strokeWeight(2);
+    rect(width/10,yTree-width/6,width/5,height/3);
+    rect(width/10,yTree+width/6,width/5,height/3);
+    rect(width/10,yTree+height/2,width/5,height/3);
+    rect(width/10,yTree+(height*5/6),width/5,height/3);
 }
   public void settings() {  size(500,750); }
   static public void main(String[] passedArgs) {
