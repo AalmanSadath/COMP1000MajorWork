426383217f36a5a176fbee83d654b262ebe0659b
AwaerSuperCC <arduinoatwork@gmail.com>
2022-10-30 03:12:39 +1100

    Completed!

--- COMP1000MajorWork.pde
+++ COMP1000MajorWork.pde
@@ -1,26 +1,33 @@
 float xCord=250;
+float yCord=675;
 float yEnemy=-20;
 float xEnemy=250;
 float walkRate=0;
 float yTree=0;
+int points=0;
 
 float[] scaleArr= new float[8];
 float[] radiusArr= new float[8];
 int[] bulbsArr=new int[8];
 int[] treeHeights={-1,1,3,5};
 int[][] pedArr=new int[6][3];
+int[][] primeBottles=new int[3][2];
 
 void setup() {
     size(500,750);
     for(int i=0;i<=7;i++){
-        scaleArr[i]=random(10, 50);
+        scaleArr[i]=random(20, 50);
         radiusArr[i]=random(10, 40);
         bulbsArr[i]=int(random(3,10));
     }
     for(int i=0;i<=5;i++){
-        pedArr[i][0]=int(random(width/5+25,(width*4/5)-25));
-        pedArr[i][1]=-20;
-        pedArr[i][2]=int(random(2,5));
+        pedArr[i][0]=int(random(width/5+25,(width*4/5)-25)); //x coordinate of ped
+        pedArr[i][1]=-20; //y coordinate of ped starts offscreen to provide smoothness
+        pedArr[i][2]=int(random(2,5)); //speed of ped
+    }
+    for(int i=0;i<=2;i++){
+        primeBottles[i][0]=int(random((width/5)+19,(width*4/5)-19));
+        primeBottles[i][1]=i*(-250);
     }
 }
 
@@ -28,26 +35,32 @@ void setup() {
 void draw() {
     wallysWalk();
     genGrass();
-    if (yTree>=height/3) {
+    genPrime();
+    if (yTree>=height/3) { //4th grass "tile" goes of screen
         treeTransfer();
         yTree=0;
     }
     yTree++;
     walkRate++;
-    if (walkRate>=50) { 
+    if (walkRate>=50) { //bottom most row of wallys walk goes off screen
         walkRate=0;
     }
     player();
     ped();
     if (collisionPed() == true) {
-        fill(100,100,100,98);
+        fill(100,100,100,99.999);
         rect(0,0,width,height);
+        fill(100);
+        rect(0,0,500,300);
         textAlign(CENTER);
         fill(0);
         textSize(70);
         text("Game Over",250,100);
+        text("Points : ",200,250);
+        text(points,370,250);
         noLoop();
     }
+    primeCollison();
 }
 
 void player() {
@@ -56,7 +69,7 @@ void player() {
     strokeWeight(3);
     fill(100);
     rectMode(CENTER);
-    rect(xCord,675,30,100);
+    rect(xCord,yCord,30,100,20);
     //personarms
     fill(0,150,0);
     strokeWeight(1);
@@ -66,12 +79,12 @@ void player() {
     //personbody
     rect(xCord-10,650,20,50,10);
     //head
-    circle(xCord-3,675,25);
+    circle(xCord-3,yCord,25);
 }
 
 void ped() {
     for(int i=0;i<=5;i++){
-        pedArr[i][1]+=pedArr[i][2];
+        pedArr[i][1]+=pedArr[i][2]; //speed of ped gets added to ycoordinate of ped
         strokeWeight(2);
         fill(200,100,100);
         rectMode(CENTER);
@@ -79,7 +92,7 @@ void ped() {
         rectMode(CORNER);
         fill(200,100,100);
         circle(pedArr[i][0],pedArr[i][1]+3,25);
-        if (pedArr[i][1] > height+10) {
+        if (pedArr[i][1] > height+10) { //ped goes offscreen
             pedArr[i][0]=int(random(width/5+25,(width*4/5)-25));
             pedArr[i][1]=-20;
             pedArr[i][2]=int(random(2,5));
@@ -88,19 +101,19 @@ void ped() {
 }
 
 void keyPressed() {
-    if (keyCode == 37 | keyCode == 65) {
-        xCord=constrain(xCord-5,115,385);
+    if (keyCode == 37 | keyCode == 65) { //checks for "a" key and left key
+        xCord=constrain(xCord-10,115,385);
     }
-    else if (keyCode == 39 | keyCode == 68) {
-        xCord=constrain(xCord+5,115,385);
+    else if (keyCode == 39 | keyCode == 68) { //checks for "d" key and right key
+        xCord=constrain(xCord+10,115,385);
     }
 }
 
 boolean collisionPed() {
     for(int i=0;i<=5;i++){
         float distX=abs(xCord-pedArr[i][0]);
-        float distY=abs(675-pedArr[i][1]);
-        if (distX <= 40 && distY <= 60) {
+        float distY=abs(yCord-pedArr[i][1]);
+        if (distX <= 39 && distY <= 59) { //little less than sum of half width and half hieght to account for rounded edges on skateboard
             return true;
         }
     }
@@ -112,7 +125,7 @@ void wallysWalk() {
     fill(193,193,171);
     strokeWeight(2);
     stroke(0);
-    for (float rows=0; rows<16; rows++) {
+    for (float rows=0; rows<16; rows++) { //one extra row offscreen for smoothness of transition
         for (float columns=0; columns<6; columns++) {
             rect(width/5+(columns*50),walkRate+(50*rows)-50,50,50);
         }
@@ -124,14 +137,14 @@ void genGrass() {
     for(int i=0;i<=3;i++){
         for(int j=1; j<=9;j+=8){
 
-            if(j==1){
+            if(j==1){ //left side
                 fill(0,155,0);
                 noStroke();
                 rect(width*j/10,yTree+(height*treeHeights[i]/6),width/5,height/3);
                 drawTree(width*j/10,int(yTree+(height*treeHeights[i]/6)),scaleArr[i],bulbsArr[i],radiusArr[i]);
             }
 
-            else if(j==9){
+            else if(j==9){ //right side
                 fill(0,155,0);
                 noStroke();
                 rect(width*j/10,yTree+(height*treeHeights[i]/6),width/5,height/3);
@@ -144,18 +157,18 @@ void genGrass() {
 void drawTree(int x, int y, float scale, int bulbs, float radius){
     for(int i=1;i<=bulbs;i++){
         int n=i-1;
-        float xCordBranch=radius*cos(2*n*3.14/bulbs)+x;
-        float yCordBranch=radius*sin(2*n*3.14/bulbs)+y;
+        float xCordBranch=radius*cos(2*n*3.14/bulbs)+x; //gets xcoordinates of the tip of a certain branch
+        float yCordBranch=radius*sin(2*n*3.14/bulbs)+y; //gets y ^^
         strokeWeight(5);
         stroke(79, 57, 9);
         line(x,y,xCordBranch,yCordBranch);
         strokeWeight(0);
         fill(22, 51, 11);
-        circle(xCordBranch,yCordBranch,scale);
+        circle(xCordBranch,yCordBranch,scale); //scale decides radius of leaves
     }
 }
 
-void treeTransfer(){
+void treeTransfer(){ //when 4th tile moves off screen, all characteristics of trees are moved one index above in array
     for(int i=3;i>0;i--){
         scaleArr[i]=scaleArr[i-1];
         scaleArr[i+4]=scaleArr[i+3];
@@ -164,9 +177,34 @@ void treeTransfer(){
         bulbsArr[i]=bulbsArr[i-1];
         bulbsArr[i+4]=bulbsArr[i+3];
     }
-    for(int i=0;i<=4;i+=4){
-        scaleArr[i]=random(10, 50);
-        radiusArr[i]=random(10, 40);
+    for(int i=0;i<=4;i+=4){ //random gen characteristics for new trees on left and right
+        scaleArr[i]=random(20, 50);
+        radiusArr[i]=random(10, 50);
         bulbsArr[i]=int(random(3,10));
+    }
+}
+
+void genPrime(){
+    for(int i=0;i<=2;i++){
+        fill(200);
+        strokeWeight(1);
+        circle(primeBottles[i][0],primeBottles[i][1],38);
+        textAlign(CENTER);
+        textSize(12);
+        fill(0);
+        text("PRIME",primeBottles[i][0],primeBottles[i][1]+5);
+        primeBottles[i][1]++;
+    }
+}
+
+void primeCollison(){
+    for(int i=0;i<=2;i++){
+        float distX=abs(xCord-primeBottles[i][0]);
+        float distY=abs(yCord-primeBottles[i][1]);
+        if (distX <= 34 && distY <= 69) {
+            primeBottles[i][0]=int(random((width/5)+19,(width*4/5)-19));
+            primeBottles[i][1]=i*(-250);
+            points++;
+        }
     }
 }
--- out/source/COMP1000MajorWork.java
+++ out/source/COMP1000MajorWork.java
@@ -15,28 +15,35 @@ import java.io.IOException;
 public class COMP1000MajorWork extends PApplet {
 
 float xCord=250;
+float yCord=675;
 float yEnemy=-20;
 float xEnemy=250;
 float walkRate=0;
 float yTree=0;
+int points=0;
 
 float[] scaleArr= new float[8];
 float[] radiusArr= new float[8];
 int[] bulbsArr=new int[8];
 int[] treeHeights={-1,1,3,5};
 int[][] pedArr=new int[6][3];
+int[][] primeBottles=new int[3][2];
 
 public void setup() {
     
     for(int i=0;i<=7;i++){
-        scaleArr[i]=random(10, 50);
+        scaleArr[i]=random(20, 50);
         radiusArr[i]=random(10, 40);
         bulbsArr[i]=PApplet.parseInt(random(3,10));
     }
     for(int i=0;i<=5;i++){
-        pedArr[i][0]=PApplet.parseInt(random(width/5+25,(width*4/5)-25));
-        pedArr[i][1]=-20;
-        pedArr[i][2]=PApplet.parseInt(random(2,5));
+        pedArr[i][0]=PApplet.parseInt(random(width/5+25,(width*4/5)-25)); //x coordinate of ped
+        pedArr[i][1]=-20; //y coordinate of ped starts offscreen to provide smoothness
+        pedArr[i][2]=PApplet.parseInt(random(2,5)); //speed of ped
+    }
+    for(int i=0;i<=2;i++){
+        primeBottles[i][0]=PApplet.parseInt(random((width/5)+19,(width*4/5)-19));
+        primeBottles[i][1]=i*(-250);
     }
 }
 
@@ -44,26 +51,32 @@ public void setup() {
 public void draw() {
     wallysWalk();
     genGrass();
-    if (yTree>=height/3) {
+    genPrime();
+    if (yTree>=height/3) { //4th grass "tile" goes of screen
         treeTransfer();
         yTree=0;
     }
     yTree++;
     walkRate++;
-    if (walkRate>=50) { 
+    if (walkRate>=50) { //bottom most row of wallys walk goes off screen
         walkRate=0;
     }
     player();
     ped();
     if (collisionPed() == true) {
-        fill(100,100,100,98);
+        fill(100,100,100,99.999f);
         rect(0,0,width,height);
+        fill(100);
+        rect(0,0,500,300);
         textAlign(CENTER);
         fill(0);
         textSize(70);
         text("Game Over",250,100);
+        text("Points : ",200,250);
+        text(points,370,250);
         noLoop();
     }
+    primeCollison();
 }
 
 public void player() {
@@ -72,7 +85,7 @@ public void player() {
     strokeWeight(3);
     fill(100);
     rectMode(CENTER);
-    rect(xCord,675,30,100);
+    rect(xCord,yCord,30,100,20);
     //personarms
     fill(0,150,0);
     strokeWeight(1);
@@ -82,12 +95,12 @@ public void player() {
     //personbody
     rect(xCord-10,650,20,50,10);
     //head
-    circle(xCord-3,675,25);
+    circle(xCord-3,yCord,25);
 }
 
 public void ped() {
     for(int i=0;i<=5;i++){
-        pedArr[i][1]+=pedArr[i][2];
+        pedArr[i][1]+=pedArr[i][2]; //speed of ped gets added to ycoordinate of ped
         strokeWeight(2);
         fill(200,100,100);
         rectMode(CENTER);
@@ -95,7 +108,7 @@ public void ped() {
         rectMode(CORNER);
         fill(200,100,100);
         circle(pedArr[i][0],pedArr[i][1]+3,25);
-        if (pedArr[i][1] > height+10) {
+        if (pedArr[i][1] > height+10) { //ped goes offscreen
             pedArr[i][0]=PApplet.parseInt(random(width/5+25,(width*4/5)-25));
             pedArr[i][1]=-20;
             pedArr[i][2]=PApplet.parseInt(random(2,5));
@@ -104,19 +117,19 @@ public void ped() {
 }
 
 public void keyPressed() {
-    if (keyCode == 37 | keyCode == 65) {
-        xCord=constrain(xCord-5,115,385);
+    if (keyCode == 37 | keyCode == 65) { //checks for "a" key and left key
+        xCord=constrain(xCord-10,115,385);
     }
-    else if (keyCode == 39 | keyCode == 68) {
-        xCord=constrain(xCord+5,115,385);
+    else if (keyCode == 39 | keyCode == 68) { //checks for "d" key and right key
+        xCord=constrain(xCord+10,115,385);
     }
 }
 
 public boolean collisionPed() {
     for(int i=0;i<=5;i++){
         float distX=abs(xCord-pedArr[i][0]);
-        float distY=abs(675-pedArr[i][1]);
-        if (distX <= 40 && distY <= 60) {
+        float distY=abs(yCord-pedArr[i][1]);
+        if (distX <= 39 && distY <= 59) { //little less than sum of half width and half hieght to account for rounded edges on skateboard
             return true;
         }
     }
@@ -128,7 +141,7 @@ public void wallysWalk() {
     fill(193,193,171);
     strokeWeight(2);
     stroke(0);
-    for (float rows=0; rows<16; rows++) {
+    for (float rows=0; rows<16; rows++) { //one extra row offscreen for smoothness of transition
         for (float columns=0; columns<6; columns++) {
             rect(width/5+(columns*50),walkRate+(50*rows)-50,50,50);
         }
@@ -140,14 +153,14 @@ public void genGrass() {
     for(int i=0;i<=3;i++){
         for(int j=1; j<=9;j+=8){
 
-            if(j==1){
+            if(j==1){ //left side
                 fill(0,155,0);
                 noStroke();
                 rect(width*j/10,yTree+(height*treeHeights[i]/6),width/5,height/3);
                 drawTree(width*j/10,PApplet.parseInt(yTree+(height*treeHeights[i]/6)),scaleArr[i],bulbsArr[i],radiusArr[i]);
             }
 
-            else if(j==9){
+            else if(j==9){ //right side
                 fill(0,155,0);
                 noStroke();
                 rect(width*j/10,yTree+(height*treeHeights[i]/6),width/5,height/3);
@@ -160,18 +173,18 @@ public void genGrass() {
 public void drawTree(int x, int y, float scale, int bulbs, float radius){
     for(int i=1;i<=bulbs;i++){
         int n=i-1;
-        float xCordBranch=radius*cos(2*n*3.14f/bulbs)+x;
-        float yCordBranch=radius*sin(2*n*3.14f/bulbs)+y;
+        float xCordBranch=radius*cos(2*n*3.14f/bulbs)+x; //gets xcoordinates of the tip of a certain branch
+        float yCordBranch=radius*sin(2*n*3.14f/bulbs)+y; //gets y ^^
         strokeWeight(5);
         stroke(79, 57, 9);
         line(x,y,xCordBranch,yCordBranch);
         strokeWeight(0);
         fill(22, 51, 11);
-        circle(xCordBranch,yCordBranch,scale);
+        circle(xCordBranch,yCordBranch,scale); //scale decides radius of leaves
     }
 }
 
-public void treeTransfer(){
+public void treeTransfer(){ //when 4th tile moves off screen, all characteristics of trees are moved one index above in array
     for(int i=3;i>0;i--){
         scaleArr[i]=scaleArr[i-1];
         scaleArr[i+4]=scaleArr[i+3];
@@ -180,10 +193,35 @@ public void treeTransfer(){
         bulbsArr[i]=bulbsArr[i-1];
         bulbsArr[i+4]=bulbsArr[i+3];
     }
-    for(int i=0;i<=4;i+=4){
-        scaleArr[i]=random(10, 50);
-        radiusArr[i]=random(10, 40);
+    for(int i=0;i<=4;i+=4){ //random gen characteristics for new trees on left and right
+        scaleArr[i]=random(20, 50);
+        radiusArr[i]=random(10, 50);
         bulbsArr[i]=PApplet.parseInt(random(3,10));
+    }
+}
+
+public void genPrime(){
+    for(int i=0;i<=2;i++){
+        fill(200);
+        strokeWeight(1);
+        circle(primeBottles[i][0],primeBottles[i][1],38);
+        textAlign(CENTER);
+        textSize(12);
+        fill(0);
+        text("PRIME",primeBottles[i][0],primeBottles[i][1]+5);
+        primeBottles[i][1]++;
+    }
+}
+
+public void primeCollison(){
+    for(int i=0;i<=2;i++){
+        float distX=abs(xCord-primeBottles[i][0]);
+        float distY=abs(yCord-primeBottles[i][1]);
+        if (distX <= 34 && distY <= 69) {
+            primeBottles[i][0]=PApplet.parseInt(random((width/5)+19,(width*4/5)-19));
+            primeBottles[i][1]=i*(-250);
+            points++;
+        }
     }
 }
   public void settings() {  size(500,750); }
